ce0e4ac0b0e094c1ec4d58f1a359918ed2525762
jared <jaredpattison@users.noreply.github.com>
2019-01-08 20:13:50 -0800

    tuesday warmup

--- index.js
+++ index.js
@@ -3,45 +3,43 @@
 const express = require('express');
 const app = express();
 
-// EJS Template Setup
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-// Static Routes
-app.use( express.static(`${__dirname}/public`))
-
-// App Middleware
+app.use( express.static(`${__dirname}/../public`));
 
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send(`<h1>Hello From /</h1>`);
+app.get ('/', (req, res) => {
+  res.send('<h1>Hey there</h1>');
 });
 
-app.post('save', (req, res) => {
+app.post('/save', (req,res) => {
   res.json(req.body);
 });
 
-app.get('/err', (req, res, next) => {
-  next('This is a catastrophic error');
+app.get('/err', (req,res,next) => {
+  next('big trouble');
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req,res) => {
   res.status(404);
-  res.statusMessage = 'Not Found';
-  res.render('not-found', {request: req});
+  res.statusMessage = 'No findy';
+  res.render('not-found', {request:req});
 });
 
-app.use( (err, req, res, next) => {
+app.use ( (err, req,res,next) => {
   res.status(500);
   res.statusMessage = 'Server Error';
-  res.render('error', {request: req, error: err});
+  res.render('error', {request:req, error:err});
 });
 
-app.listen(8080, () => console.log('Server up on 8080'));
-
-
-
+module.exports = {
+  server: app,
+  start: () => {
+    const PORT = process.env.port || 3000;
+    app.listen(PORT, () => console.log(`server up on ${PORT}`));
+  },
+};
 
 
